f76dffbec29c8a2e677c2af87cc7ce4a9e04cbaf
sidmaskey13 <sid.mas13@gmail.com>
2020-07-19 23:54:12 +0545

    slight change

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import index,show
 app_name='blog'
 urlpatterns=[
-    path('', index, name='name'),
+    path('', index, name='index'),
     path('<int:article_id>/', show, name='show'),
 ]
